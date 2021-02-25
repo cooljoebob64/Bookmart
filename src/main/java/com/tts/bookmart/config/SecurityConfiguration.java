a15f3560de7d13421c5e30caf0dfa504ca475d15
Joshua Luppes <cooljoebob64@gmail.com>
2021-02-25 16:13:31 -0600

    Fix signup form

--- src/main/java/com/tts/bookmart/config/SecurityConfiguration.java
+++ src/main/java/com/tts/bookmart/config/SecurityConfiguration.java
@@ -36,6 +36,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/console/**").permitAll()
+                .antMatchers("/").permitAll()
                 .antMatchers("/about").permitAll()
                 .antMatchers("/main").permitAll()
                 .antMatchers("/product").permitAll()
@@ -43,16 +44,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/custom.js").permitAll()
                 .antMatchers("/custom.css").permitAll()
                 .antMatchers("/cart").authenticated()
-                .antMatchers().hasAuthority("USER").anyRequest()
-                .authenticated().and().csrf().disable().formLogin()
+            .and().formLogin()
                 .loginPage("/signin").failureUrl("/signin?error=true")
                 .defaultSuccessUrl("/")
-                .and().formLogin()
-                .loginPage("/signin")
-                .loginProcessingUrl("/signin")
-                .and().logout()
+                .loginProcessingUrl("/login")
+            .and().logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/signout"))
-                .logoutSuccessUrl("/").and().exceptionHandling();
+                .logoutSuccessUrl("/")
+//            .and().exceptionHandling()
+        ;
         http.headers().frameOptions().disable();
     }
 }
