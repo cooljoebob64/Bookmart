b810afa69f9beae499489474ea96246658c5e7ca
Joshua Luppes <cooljoebob64@gmail.com>
2021-02-26 16:15:46 -0600

    Add cart item details to Cart template

--- src/main/java/com/tts/bookmart/controller/CartController.java
+++ src/main/java/com/tts/bookmart/controller/CartController.java
@@ -6,6 +6,7 @@ import com.tts.bookmart.service.ProductService;
 import com.tts.bookmart.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -40,7 +41,9 @@ public class CartController {
     }
 
     @GetMapping("/cart")
-    public String showCart() {
+    public String showCart(Model model)
+    {
+        model.addAttribute("cart", cart());
         return "cart";
     }
 
