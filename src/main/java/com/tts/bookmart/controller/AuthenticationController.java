5e0ab57b5ce255bc276cee53561d1b12b80d8af7
Joshua Luppes <cooljoebob64@gmail.com>
2021-02-26 08:19:55 -0600

    Add some features for future field validation

--- src/main/java/com/tts/bookmart/controller/AuthenticationController.java
+++ src/main/java/com/tts/bookmart/controller/AuthenticationController.java
@@ -4,6 +4,7 @@ import com.tts.bookmart.model.User;
 import com.tts.bookmart.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,20 +20,32 @@ class AuthenticationController {
     private UserService userService;
 
     @GetMapping("/signin")
-    public String login() {
+    public String login(Model model) {
+        User user = new User();
+        model.addAttribute(user);
         return "signin";
     }
 
     @PostMapping("/signin")
     public String signup(@Valid User user, @RequestParam String submit, BindingResult bindingResult,
-                         HttpServletRequest request) throws ServletException {
+                         HttpServletRequest request, Model model) throws ServletException {
+        model.addAttribute("user", new User());
+
         String password = user.getPassword();
         if (submit.equals("up")) {
-            if (userService.findByUsername(user.getUsername()) == null) {
-                userService.saveNew(user);
-            } else {
-                bindingResult.rejectValue("username", "error.user", "Username is already taken.");
+//            if (userService.findByUsername(user.getUsername()) == null) {
+//                userService.saveNew(user);
+//            } else {
+//                bindingResult.rejectValue("username", "error.user", "Username is already taken.");
+//                return "signin";
+//            }
+            User userExists = userService.findByUsername(user.getUsername());
+            if (userExists != null) {
+                bindingResult.rejectValue("username", "error.user", "Username is already taken");
                 return "signin";
+            }
+            if (!bindingResult.hasErrors()) {
+                userService.saveNew(user);
             }
         }
         request.login(user.getUsername(), password);
